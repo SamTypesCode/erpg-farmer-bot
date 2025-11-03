98b5e192e6c3a3312271e079be7d0f6cb0e3eb5a
Sampreet Singh <preet.samsam@gmail.com>
2025-11-04 00:00:19 +0530

    feat: read tokens from text file & login all bots concurrently

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,29 @@
-console.log("Hello world");
+import path from "path";
+import fs from "fs/promises";
+import { fileURLToPath } from "url";
+import { Client } from "discord.js-selfbot-v13";
+
+(async () => {
+  const tokens: string[] = (
+    await fs.readFile(
+      path.join(
+        path.dirname(fileURLToPath(import.meta.url)),
+        "../config/tokens.txt"
+      ),
+      "utf-8"
+    )
+  )
+    .split(/\r?\n/)
+    .map((line) => line.trim())
+    .filter(Boolean);
+
+  for (const token of tokens) {
+    const client = new Client();
+
+    client.on("ready", () => {
+      console.log(`${client.user?.username} is ready!`);
+    });
+
+    client.login(token);
+  }
+})();
